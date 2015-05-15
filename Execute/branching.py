cec8fbab5bf39a7b51486c246370815ef1de0d31
tdpearson <tdpearson@users.noreply.github.com>
2015-05-15 18:21:08 -0500

    fixed assignment of thumb_bit

--- Execute/branching.py
+++ Execute/branching.py
@@ -53,7 +53,7 @@ class Branching():
         if args['encoding'] == 'A2-I':  # BLX
             self.registers.LR = self.registers.PC - 4
             self.registers.PC += signed_int((args['imm24'] << 1 | args['H']) << 1)
-            self.process_mode.CPSR.thumb_bit == 1
+            self.process_mode.CPSR.thumb_bit = 1
             print('branched to %s with link back at %s and mode change to Thumb' % (hex(self.registers.PC), hex(self.registers.LR)))
         if args['encoding'] == 'T1-I':
             self.registers.LR = self.registers.PC - 2
@@ -68,7 +68,7 @@ class Branching():
             I2 = 1 ^ (args['J2'] ^ args['S'])
             delta = signed_int((args['S'] << 22 | I1 << 21 | I2 << 20 | args['imm10H'] << 10 | args['imm10L']) << 2)
             self.registers.PC += delta
-            self.process_mode.CPSR.thumb_bit == 0
+            self.process_mode.CPSR.thumb_bit = 0
             print('branched to %s with link back at %s and mode change to Arm' % (hex(self.registers.PC), hex(self.registers.LR)))
 
     def BX():
